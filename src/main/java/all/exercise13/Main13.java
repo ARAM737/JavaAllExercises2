60c0a3627de98dcfbc688402c1804f4254e3bcaa
ARAM <aramkenny97@gmail.com>
2021-04-22 13:34:57 +0300

    Done

---
+++ src/main/java/all/exercise13/Main13.java
@@ -0,0 +1,13 @@
+package all.exercise13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main13 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main13.class, args);
+        Student student = new Student();
+
+    }
+}
